5c6ff749f0aa5a33777338a7fdde291e91a24f77
Ilya Zaytsev <ilya@zaytsev.xyz>
2020-10-17 23:31:14 +0300

    http server

--- app.js
+++ app.js
@@ -7,3 +7,33 @@ const path = require('path')
 const fs = require('fs')
 
 const app = express()
+
+app.use(express.json({ extended: true }))
+
+if (process.env.NODE_ENV === 'production') {
+
+}
+
+const PORT = config.get('PORT');
+
+async function start() {
+	try {
+		await mongoose.connect(config.get('MONGO_URI'), {
+			useNewUrlParser: true,
+			useUnifiedTopology: true,
+			useCreateIndex: true
+		})
+
+		http.createServer(app).listen(PORT, () => {
+			console.log('[HTTP] App has been started on port 80')
+		})
+
+		// https server
+	} catch (err) {
+		console.log('Server error', err.message)
+		process.exit(1)
+	}
+}
+
+start()
+	.then(() => console.log('Success'))
