8c1f29f2dcfc03181127094008f6a965ed5e34ab
Ilya Zaytsev <ilya@zaytsev.xyz>
2020-10-18 02:01:58 +0300

    i dont know i just want to sleep sorry

--- app.js
+++ app.js
@@ -11,7 +11,11 @@ const app = express()
 app.use(express.json({ extended: true }))
 
 if (process.env.NODE_ENV === 'production') {
+	app.use('/', express.static(path.join(__dirname, 'client', 'build')))
 
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+	})
 }
 
 const PORT = config.get('PORT');
