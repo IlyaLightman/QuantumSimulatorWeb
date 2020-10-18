ab28c08d72abd3fa92d96a88e6e16a669e420705
Ilya Zaytsev <ilya@zaytsev.xyz>
2020-10-18 23:36:48 +0300

    quantum simulator page setup

---
+++ client/src/containers/QuantumSimulatorPage/QuantumSimulatorPage.js
@@ -0,0 +1,16 @@
+import React, { useState } from 'react'
+import './QuantumSimulatorPage.scss'
+
+const QuantumSimulatorPage = () => {
+	const [state, setState] = useState({
+		qubits: 1, repeats: 1000, instructions: []
+	})
+
+	return (
+		<div>
+
+		</div>
+	)
+}
+
+export default QuantumSimulatorPage
